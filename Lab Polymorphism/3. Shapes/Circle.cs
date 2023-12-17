38165187d5ca959600ab3259bed40bf44d944c0f
metalonov <118399788+metalonov@users.noreply.github.com>
2023-12-17 06:20:00 +0200

    Lab Polymorphism

---
+++ Lab Polymorphism/3. Shapes/Circle.cs
@@ -0,0 +1,49 @@
+﻿namespace Shapes;
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double Radius
+    {
+        get { return radius; }
+        private set { radius = value; }
+    }
+
+
+    public void Draw()
+    {
+        double rin = this.Radius - 0.4;
+        double rout = this.Radius + 0.4;
+        for (double y = Radius; y >= -Radius; --y)
+        {
+            for (double x = -Radius; x < rout; x += 0.5)
+            {
+                double value = x * x + y * y;
+                if (value >= rin * rin && value <= rout * rout)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ Lab Polymorphism/3. Shapes/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿namespace Shapes;
+
+public class Rectangle : Shape
+{
+    private int height;
+    private int width;
+
+    public int Width
+    {
+        get { return width; }
+        private set { width = value; }
+    }
+
+
+    public int Height
+    {
+        get { return height; }
+        private set { height = value; }
+    }
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+
+    public void Draw()
+    {
+        DrawLine(Width, '*', '*');
+        for (int i = 1; i < Height - 1; ++i)
+        {
+            DrawLine(Width, '*', ' ');
+        }
+        DrawLine(Width, '*', '*');
+    }
+
+    private void DrawLine(int width, char end, char mid)
+    {
+        Console.Write(end);
+        for (int i = 1; i < width - 1; ++i)
+        {
+            Console.Write(mid);
+        }
+        Console.WriteLine(end);
+    }
+    public override double CalculateArea()
+    {
+        return width * width;
+    }
+
+    public override double CalculatePerimeter()
+    {
+        return 2 * (width + width);
+    }
+}
